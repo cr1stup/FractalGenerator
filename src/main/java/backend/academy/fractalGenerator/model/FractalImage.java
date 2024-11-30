de1714f089ec95b7475839536a89eac6b5d05cef
cr1stup <scaleupgo@gmail.com>
2024-11-30 18:24:31 +0300

    feat(model): add method create to FractalImage class

--- src/main/java/backend/academy/fractalGenerator/model/FractalImage.java
+++ src/main/java/backend/academy/fractalGenerator/model/FractalImage.java
@@ -1,3 +1,14 @@
 package backend.academy.fractalGenerator.model;
 
-public record FractalImage(Pixel[] data, int width, int height) {}
+public record FractalImage(Pixel[] data, int width, int height) {
+
+    public static FractalImage create(int width, int height) {
+        Pixel[] data = new Pixel[width * height];
+
+        for (int i = 0; i < height * width; i++) {
+            data[i] = new Pixel(0, 0, 0, 0);
+        }
+
+        return new FractalImage(data, width, height);
+    }
+}
