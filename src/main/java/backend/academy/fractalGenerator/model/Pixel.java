1729136a31b2f496fdd9e4946fe2767705669082
cr1stup <scaleupgo@gmail.com>
2024-11-30 18:20:34 +0300

    feat(model): implements Pixel class with methods
    add fields and methods for working with pixel to Pixel class

--- src/main/java/backend/academy/fractalGenerator/model/Pixel.java
+++ src/main/java/backend/academy/fractalGenerator/model/Pixel.java
@@ -1,4 +1,30 @@
 package backend.academy.fractalGenerator.model;
 
+import java.awt.Color;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class Pixel {
+    private int red;
+    private int green;
+    private int blue;
+    private int hitCount;
+
+    public void saturateHitCount(Color color) {
+        if (hitCount == 0) {
+            setRGB(color.getRed(), color.getGreen(), color.getBlue());
+        } else {
+            red = (red + color.getRed()) / 2;
+            green = (green + color.getGreen()) / 2;
+            blue = (blue + color.getBlue()) / 2;
+        }
+
+        hitCount++;
+    }
+
+    private void setRGB(int r, int g, int b) {
+        this.red = r;
+        this.green = g;
+        this.blue = b;
+    }
 }
