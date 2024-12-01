8b855f2b7e4bc3ec873b3d2c1c750c9556cc8256
cr1stup <scaleupgo@gmail.com>
2024-12-01 12:44:16 +0300

    feat(renderer): add method to render one sample

--- src/main/java/backend/academy/fractalGenerator/renderer/BaseRenderer.java
+++ src/main/java/backend/academy/fractalGenerator/renderer/BaseRenderer.java
@@ -2,10 +2,16 @@ package backend.academy.fractalGenerator.renderer;
 
 import backend.academy.fractalGenerator.model.AffineCoefficient;
 import backend.academy.fractalGenerator.model.FractalImage;
+import backend.academy.fractalGenerator.model.Pixel;
+import backend.academy.fractalGenerator.model.Point;
 import backend.academy.fractalGenerator.model.Rect;
 import backend.academy.fractalGenerator.transformations.AffineTransformation;
 import backend.academy.fractalGenerator.transformations.Transformation;
+import backend.academy.fractalGenerator.utils.FractalImageUtils;
+import backend.academy.fractalGenerator.utils.ListUtils;
+import backend.academy.fractalGenerator.utils.RectUtils;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
@@ -36,6 +42,40 @@ public abstract class BaseRenderer implements Renderer {
         Rect world,
         List<AffineTransformation> affineTransformations
     );
+
+    protected void renderOneSample(FractalImage image, Rect world, List<AffineTransformation> affineTransformations) {
+        Point currentPoint = RectUtils.randomPoint(world);
+
+        for (int step = INIT_STEPS; step < iterPerSample; step++) {
+            AffineTransformation affine = ListUtils.random(affineTransformations);
+            Transformation transformation = ListUtils.random(transformations);
+            currentPoint = affine.apply(currentPoint);
+            currentPoint = transformation.apply(currentPoint);
+
+            if (step > 0) {
+                double theta = 0.0;
+                for (int piece = 0; piece < symmetry; theta += Math.PI * 2 / symmetry, piece++) {
+                    Point point = RectUtils.rotatePoint(world, currentPoint, theta);
+                    processPoint(world, image, point, affine);
+                }
+            }
+        }
+    }
+
+    private void processPoint(Rect world, FractalImage image, Point point, AffineTransformation affine) {
+        if (!world.contains(point)) {
+            return;
+        }
+
+        Pixel pixel = FractalImageUtils.resolvePixel(world, point, image);
+
+        if (pixel != null) {
+            synchronized (pixel) {
+                Color color = affine.affineCoefficient().color();
+                pixel.saturateHitCount(color);
+            }
+        }
+    }
 
     private List<AffineTransformation> generateAffineTransformations() {
         List<AffineTransformation> affineTransformations = new ArrayList<>(affineCount);
