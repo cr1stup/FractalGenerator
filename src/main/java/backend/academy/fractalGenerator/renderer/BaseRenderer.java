0ab7b499940cb236a2a000a4c333cdcd179bf487
cr1stup <scaleupgo@gmail.com>
2024-12-01 12:10:03 +0300

    feat(renderer): implement render method in BaseRenderer

--- src/main/java/backend/academy/fractalGenerator/renderer/BaseRenderer.java
+++ src/main/java/backend/academy/fractalGenerator/renderer/BaseRenderer.java
@@ -1,12 +1,18 @@
 package backend.academy.fractalGenerator.renderer;
 
+import backend.academy.fractalGenerator.model.AffineCoefficient;
 import backend.academy.fractalGenerator.model.FractalImage;
 import backend.academy.fractalGenerator.model.Rect;
+import backend.academy.fractalGenerator.transformations.AffineTransformation;
 import backend.academy.fractalGenerator.transformations.Transformation;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
+@SuppressFBWarnings
 public abstract class BaseRenderer implements Renderer {
 
     protected final int affineCount;
@@ -18,6 +24,29 @@ public abstract class BaseRenderer implements Renderer {
 
     @Override
     public FractalImage render(int width, int height, Rect world) {
-        return null;
+        FractalImage image = FractalImage.create(width, height);
+        List<AffineTransformation> affineTransformations = generateAffineTransformations();
+        renderAllImage(image, world, affineTransformations);
+
+        return image;
+    }
+
+    protected abstract void renderAllImage(
+        FractalImage image,
+        Rect world,
+        List<AffineTransformation> affineTransformations
+    );
+
+    private List<AffineTransformation> generateAffineTransformations() {
+        List<AffineTransformation> affineTransformations = new ArrayList<>(affineCount);
+
+        for (int i = 0; i < affineCount; i++) {
+            AffineTransformation transformation = new AffineTransformation(
+                AffineCoefficient.generateRandom(ThreadLocalRandom.current())
+            );
+            affineTransformations.add(transformation);
+        }
+
+        return affineTransformations;
     }
 }
