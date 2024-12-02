8c70886211cab93fb567017835edd8d78ea08667
cr1stup <scaleupgo@gmail.com>
2024-12-02 08:10:45 +0300

    feat(generator): implement generate method

--- src/main/java/backend/academy/fractalGenerator/FractalFlameGenerator.java
+++ src/main/java/backend/academy/fractalGenerator/FractalFlameGenerator.java
@@ -1,6 +1,20 @@
 package backend.academy.fractalGenerator;
 
+import backend.academy.fractalGenerator.model.FractalImage;
+import backend.academy.fractalGenerator.model.Parameters;
+import backend.academy.fractalGenerator.processor.ImageProcessor;
+import backend.academy.fractalGenerator.saver.ImageSaver;
+import java.io.IOException;
+
 public class FractalFlameGenerator {
-    public void generate() {
+    public void generate(Parameters p) throws IOException, InterruptedException {
+        FractalImage image = p.renderer().render(p.width(), p.height(), p.area());
+
+        for (ImageProcessor processor : p.processors()) {
+            processor.process(image);
+        }
+
+        ImageSaver saver = new ImageSaver(p.format());
+        saver.save(image, p.path());
     }
 }
